b0a1721e4a3828a5aac362e6b77a5b9f042912f4
dimitri-hoareau <dimitrihoareau2@gmail.com>
2026-01-08 20:22:43 +0100

    create BusShift model and admin interface

--- padam_django/apps/geography/models.py
+++ padam_django/apps/geography/models.py
@@ -18,6 +18,50 @@ class Place(models.Model):
 class BusStop(models.Model):
     name = models.CharField("Name of the bus stop", max_length=50)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"Bus stop: {self.name} (id: {self.pk})"
+
+
+class BusShift(models.Model):
+    """Represents a bus shift (trajet de bus)"""
+    bus = models.ForeignKey('fleet.Bus', on_delete=models.CASCADE, related_name='shifts')
+    driver = models.ForeignKey('fleet.Driver', on_delete=models.CASCADE, related_name='shifts')
+    stops = models.ManyToManyField(BusStop, through='BusShiftStop', related_name='shifts')
+
+    def __str__(self):
+        return f"Shift {self.pk}: Bus {self.bus.licence_plate} - Driver {self.driver.user.username}"
+
+    @property
+    def departure_time(self):
+        """Returns the time of the first stop"""
+        first_stop = self.busshiftstop_set.first()
+        return first_stop.scheduled_time if first_stop else None
+
+    @property
+    def arrival_time(self):
+        """Returns the time of the last stop"""
+        last_stop = self.busshiftstop_set.last()
+        return last_stop.scheduled_time if last_stop else None
+
+    @property
+    def duration(self):
+        """Returns the total duration of the shift"""
+        if self.departure_time and self.arrival_time:
+            return self.arrival_time - self.departure_time
+        return None
+
+
+class BusShiftStop(models.Model):
+    """Represents a stop during a bus shift with order and time"""
+    bus_shift = models.ForeignKey(BusShift, on_delete=models.CASCADE)
+    bus_stop = models.ForeignKey(BusStop, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField("Stop order in the shift")
+    scheduled_time = models.DateTimeField("Scheduled time at this stop")
+
+    class Meta:
+        ordering = ['order']
+        unique_together = (('bus_shift', 'order'), ('bus_shift', 'bus_stop'))
+
+    def __str__(self):
+        return f"Shift {self.bus_shift.pk} - Stop {self.order}: {self.bus_stop.name} at {self.scheduled_time}"
